3f945cc76020c47a5703e58a951a088ff46ec427
Fawaz Khan R <115716567+Fawaz-Khan-R@users.noreply.github.com>
2022-11-25 21:07:57 +0530

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,10 @@
+#This python program reverses an integer and prints the value of the reversed number.
+t = int(input())
+for i in range(t):
+    reverse = 0
+    num = int(input())
+    while(num):
+        remainder = num%10
+        reverse =reverse*10+remainder
+        num = int(num/10)
+    print(reverse)
